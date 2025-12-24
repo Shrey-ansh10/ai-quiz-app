a185d0789bdb20af3b525336700a99f1c042940a
Shreyansh <shreyanshsharma1010@gmail.com>
2025-12-23 21:31:45 -0800

    App Components - binding all frontend components together via routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,14 +16,10 @@ function App() {
       <Routes> 
         <Route path="/sign-in/*" element={<AuthenticationPage/>} /> {/* creating Authentication page - so if user is at this route, we will render authentication page */}
         <Route path="/sign-up" element={<AuthenticationPage/>} ></Route> {/* clerk will handel the signup and sign in */}
-
-        <Route element={<Layout/>}> {/* This won't be self closing as -> Everything in this route will be rendered in the layout component - It will act as a parent div and all other components of out website will be in this component */}
-          { /* Now we will creates routes to various pages of out app */}
-          
-          <Route path="/" element={<ChallengeGenerator/>} />
+        <Route element={<Layout />}> {/* This won't be self closing as -> Everything in this route will be rendered in the layout component - It will act as a parent div and all other components of out website will be in this component */}
+          { /* Routes to various pages of our app */}
+          <Route path="/" element={<ChallengeGenerator />} />
           <Route path="/history" element={<HistoryPanel /> } />
-        
-
         </Route>
 
       </Routes>
