ae56d32355501ea7d8f4c7ce20e09d3bd96a60ce
Shreyansh <shreyanshsharma1010@gmail.com>
2025-12-23 21:29:09 -0800

    Layout for Sign-In, Sign-up and Child Components when the use is logged in

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -4,8 +4,32 @@ import { Outlet, Link, Navigate } from "react-router-dom"
 
 export default function Layout(){
     return (
-        <>
+        
+            <div className="app-layout">
+                <header className="app-header">
+                    <div className="header-content">
+                        <h1>Code Challenge Generator</h1>
+                        <nav>
+                            <SignedIn>
+                                <Link to="/" >Generate Challenges</Link>
+                                <Link to="/history" >History</Link>
+                                <UserButton/>
+                            </SignedIn>
+                        </nav>
+                    </div>
+                </header>
+
+                <main className="app-main" >
+                    <SignedOut>
+                        <Navigate to="/sign-in" replace />
+                    </SignedOut>
+                    <SignedIn>
+                        <Outlet /> {/** The oultlet will render other child components in the Layout when the user is logged in.  */}
+                    </SignedIn>
+                </main>
+
+                
+            </div>
             
-        </>
     )
 }
